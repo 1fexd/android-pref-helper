2f44799dd9345cf5df8ab4ef3deb9040a73011b2
1fexd <PGFvy85nC@protonmail.com>
2023-05-30 16:29:04 +0200

    Revert "feat: Make cache configurable"
    This reverts commit b3e93dfd295cc51e1b1c2e3cc95a99829bfb8375.

--- preference-helper-compose/src/main/kotlin/fe/android/preference/helper/compose/PreferenceRepositoryExt.kt
+++ preference-helper-compose/src/main/kotlin/fe/android/preference/helper/compose/PreferenceRepositoryExt.kt
@@ -3,105 +3,49 @@ package fe.android.preference.helper.compose
 import fe.android.preference.helper.BasePreference
 import fe.android.preference.helper.PreferenceRepository
 
-class PreferenceRepositoryCompose(private val preferenceRepository: PreferenceRepository) {
-    fun getStringState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.PreferenceNullable<String>
-    ) = getState(
-        cacheMode,
-        preference,
-        preferenceRepository::writeString,
-        preferenceRepository::getString
-    )
+fun PreferenceRepository.getStringState(
+    preference: BasePreference.PreferenceNullable<String>
+) = getState(preference, ::writeString, ::getString)
 
-    @JvmName("getMappedAsStateByString")
-    fun <T> getState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.MappedPreference<T, String>,
-    ) = getState(cacheMode, preference, preferenceRepository::write, preferenceRepository::get)
+@JvmName("getMappedAsStateByString")
+fun <T> PreferenceRepository.getState(
+    preference: BasePreference.MappedPreference<T, String>,
+) = getState(preference, ::write, ::get)
 
-    fun getIntState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.Preference<Int>
-    ) = getState(
-        cacheMode,
-        preference,
-        preferenceRepository::writeInt,
-        preferenceRepository::getInt
-    )
+fun PreferenceRepository.getIntState(preference: BasePreference.Preference<Int>) = getState(
+    preference, ::writeInt, ::getInt
+)
 
-    @JvmName("getMappedAsStateByInt")
-    fun <T> getState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.MappedPreference<T, Int>,
-    ) = getState(cacheMode, preference, preferenceRepository::write, preferenceRepository::get)
+@JvmName("getMappedAsStateByInt")
+fun <T> PreferenceRepository.getState(
+    preference: BasePreference.MappedPreference<T, Int>,
+) = getState(preference, ::write, ::get)
 
-    fun getLongState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.Preference<Long>
-    ) = getState(
-        cacheMode,
-        preference,
-        preferenceRepository::writeLong,
-        preferenceRepository::getLong
-    )
+fun PreferenceRepository.getLongState(preference: BasePreference.Preference<Long>) = getState(
+    preference, ::writeLong, ::getLong
+)
 
-    @JvmName("getMappedAsStateByLong")
-    fun <T> getState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.MappedPreference<T, Long>,
-    ) = getState(cacheMode, preference, preferenceRepository::write, preferenceRepository::get)
+@JvmName("getMappedAsStateByLong")
+fun <T> PreferenceRepository.getState(
+    preference: BasePreference.MappedPreference<T, Long>,
+) = getState(preference, ::write, ::get)
 
-    fun getBooleanState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.Preference<Boolean>
-    ) =
-        getState(
-            cacheMode,
-            preference,
-            preferenceRepository::writeBoolean,
-            preferenceRepository::getBoolean
-        )
+fun PreferenceRepository.getBooleanState(preference: BasePreference.Preference<Boolean>) = getState(
+    preference, ::writeBoolean, ::getBoolean
+)
 
-    @JvmName("getMappedAsStateByBoolean")
-    fun <T> getState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: BasePreference.MappedPreference<T, Boolean>,
-    ) = getState(cacheMode, preference, preferenceRepository::write, preferenceRepository::get)
+@JvmName("getMappedAsStateByBoolean")
+fun <T> PreferenceRepository.getState(
+    preference: BasePreference.MappedPreference<T, Boolean>,
+) = getState(preference, ::write, ::get)
 
-    enum class CacheMode {
-        Bypass, Cached
-    }
+private val stateCache = mutableMapOf<String, RepositoryState<*, *, *>>()
 
-    enum class CacheResult {
-        New, Cached
-    }
-
-    private val stateCache = mutableMapOf<String, RepositoryState<*, *, *>>()
-
-    fun clearCache() = stateCache.clear()
-
-    @Suppress("UNCHECKED_CAST")
-    private fun <T, NT, P : BasePreference<T, NT>> getState(
-        cacheMode: CacheMode = CacheMode.Cached,
-        preference: P,
-        writer: (P, NT) -> Unit,
-        reader: (P) -> NT,
-    ): RepositoryState<T, NT, P> {
-        if (cacheMode == CacheMode.Cached && stateCache.containsKey(preference.key)) {
-            return stateCache[preference.key] as RepositoryState<T, NT, P>
-        }
-
-        val newState = getNewState(preference, writer, reader)
-        stateCache[preference.key] = newState
-
-        return newState
-    }
-
-    private fun <T, NT, P : BasePreference<T, NT>> getNewState(
-        preference: P,
-        writer: (P, NT) -> Unit,
-        reader: (P) -> NT,
-    ) = RepositoryState(preference, writer, reader)
-}
-
+@Suppress("UNCHECKED_CAST")
+private fun <T, NT, P : BasePreference<T, NT>> getState(
+    preference: P,
+    writer: (P, NT) -> Unit,
+    reader: (P) -> NT,
+) = stateCache.getOrPut(preference.key) {
+    RepositoryState(preference, writer, reader)
+} as RepositoryState<T, NT, P>
