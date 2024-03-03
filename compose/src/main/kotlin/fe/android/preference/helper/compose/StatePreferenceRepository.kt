9483a74f4e55d9faf621ec2a2baced74437d3a02
1fexd <PGFvy85nC@protonmail.com>
2024-03-03 17:44:39 +0100

    fix: Jvm name clashes

--- compose/src/main/kotlin/fe/android/preference/helper/compose/StatePreferenceRepository.kt
+++ compose/src/main/kotlin/fe/android/preference/helper/compose/StatePreferenceRepository.kt
@@ -13,48 +13,52 @@ public abstract class StatePreferenceRepository(
     @Suppress("MemberVisibilityCanBePrivate") public val stateCache: StateCache = StateCache()
 ) : PreferenceRepository(context) {
 
+    @JvmName("asStringState")
     public fun asState(preference: Preference.Nullable<String>): StateNullablePreference<String?, String> {
         return createCachedState(preference, ::put, this::get)
     }
 
-    @JvmName("getMappedAsStateByString")
+    @JvmName("asMappedStringState")
     public fun <T : Any> asState(preference: Preference.Mapped<T, String>): StateMappedPreference<T, String> {
         return createCachedState(preference, ::put, this::get)
     }
 
+    @JvmName("asIntState")
     public fun asState(preference: Preference.Default<Int>): StatePreference<Int> {
         return createCachedState(preference, ::put, ::getInt)
     }
 
-    @JvmName("getMappedAsStateByInt")
+    @JvmName("asMappedIntState")
     public fun <T : Any> asState(preference: Preference.Mapped<T, Int>): StateMappedPreference<T, Int> {
         return createCachedState(preference, ::put, this::get)
     }
 
+    @JvmName("asLongState")
     public fun asState(preference: Preference.Default<Long>): StatePreference<Long> {
         return createCachedState(preference, ::put, this::get)
     }
 
-    @JvmName("getMappedAsStateByLong")
+    @JvmName("asMappedLongState")
     public fun <T : Any> asState(preference: Preference.Mapped<T, Long>): StateMappedPreference<T, Long> {
         return createCachedState(preference, ::put, this::get)
     }
 
+    @JvmName("asBooleanState")
     public fun asState(preference: Preference.Default<Boolean>): StatePreference<Boolean> {
         return createCachedState(preference, ::put, this::get)
     }
 
-    @JvmName("getMappedAsStateByBoolean")
+    @JvmName("asMappedBooleanState")
     public fun <T : Any> asState(preference: Preference.Mapped<T, Boolean>): StateMappedPreference<T, Boolean> {
         return createCachedState(preference, ::put, this::get)
     }
 
-    @Suppress("UNCHECKED_CAST")
     private fun <T : Any, NT, P : Preference<T, NT>> createCachedState(
         preference: P,
         put: Put<P, NT>,
         get: Get<P, NT>,
     ): MutablePreferenceState<T, NT, P> {
+        @Suppress("UNCHECKED_CAST")
         return stateCache.getOrPut(preference, put, get) as MutablePreferenceState<T, NT, P>
     }
 }
