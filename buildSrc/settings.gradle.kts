f6ab90fae8c4a73838e95811ad987ed2791d26f8
1fexd <PGFvy85nC@protonmail.com>
2024-11-08 06:52:20 +0100

    fix: Dummy

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -5,8 +5,6 @@ pluginManagement {
 
     plugins {
         id("de.fayard.refreshVersions") version "0.60.5"
-////                                # available:"0.60.4"
-////                                # available:"0.60.5"
     }
 }
 
